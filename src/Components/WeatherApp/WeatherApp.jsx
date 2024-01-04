b99150449a62dcb2faab300c619a475992aaf686
Bushaan Gunatilake <bushaangunatilake@gmail.com>
2024-01-04 22:19:53 +0530

    create the rest of the UI

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import CloudIcon from "../Assets/cloud.png";
+import HumidityIcon from "../Assets/humidity.png";
 import SearchIcon from "../Assets/search.png";
+import WindIcon from "../Assets/wind.png";
+
 import "./WeatherApp.css";
 
 const WeatherApp = () => {
@@ -9,6 +13,27 @@ const WeatherApp = () => {
         <input type="text" className="cityInput" placeholder="Search" />
         <div className="search-icon">
           <img src={SearchIcon} alt="" />
+        </div>
+      </div>
+      <div className="weather-image">
+        <img src={CloudIcon} alt="" />
+      </div>
+      <div className="weather-temp">24°c</div>
+      <div className="weather-location">London</div>
+      <div className="data-container">
+        <div className="element">
+          <img className="icon" src={HumidityIcon} alt="" />
+          <div className="data">
+            <div className="humidity-percent">64%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+        <div className="element">
+          <img className="icon" src={WindIcon} alt="" />
+          <div className="data">
+            <div className="humidity-percent">18 km/h</div>
+            <div className="text">Wind Speed</div>
+          </div>
         </div>
       </div>
     </div>
