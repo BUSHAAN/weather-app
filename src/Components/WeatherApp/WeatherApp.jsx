10e8116b0143a7dedbfb5a771792dd1e27746271
Bushaan Gunatilake <bushaangunatilake@gmail.com>
2024-01-04 22:07:40 +0530

    Create the search bar

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import "./WeatherApp.css"
+import React from "react";
+import SearchIcon from "../Assets/search.png";
+import "./WeatherApp.css";
 
 const WeatherApp = () => {
   return (
-    <div>
-
+    <div className="container">
+      <div className="top-bar">
+        <input type="text" className="cityInput" placeholder="Search" />
+        <div className="search-icon">
+          <img src={SearchIcon} alt="" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WeatherApp
+export default WeatherApp;
